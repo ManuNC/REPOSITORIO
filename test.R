2407987fd3df01732dae738a9a4ff292ee4af93b
ManuNC <m.nunez.cruzat@gmail.com>
2022-05-31 21:58:26 -0400

    test 2

--- test.R
+++ test.R
@@ -1,3 +1,7 @@
 #Prueba siguiente 
 #para evaluar lo que debemos hacer
 #en el programa
+# enviar esta prueba a grupo changolab
+# verificar el error de carga del programa
+# evaluar error 403
+# organizar viaje a santiago
